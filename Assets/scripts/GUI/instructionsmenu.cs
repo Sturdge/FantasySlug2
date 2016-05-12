4fba15c95be2e8a62cbeb2e4ab477b0c5dc505a9
Bobby raw <Kingbobsley@hotmail.co.uk>
2016-05-12 20:49:36 +0100

    Instructions for game
    made a scene holding the instructions for the game as well as explaining
    the UI

---
+++ Assets/scripts/GUI/instructionsmenu.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class instructionsmenu : MonoBehaviour
+{
+
+
+    public GameObject gameInstructionsCanvas; // holds the menu for the game instructions.
+    public GameObject uiInstructions; // holds the menu for the UI instructions.
+    public string backToMenu; // holds the level name of the main menu.
+    public string tutorial; // holds the level name of the tutorial level.
+
+    // Use this for initialization
+    void Start()
+    {
+
+        gameInstructionsCanvas.SetActive(true); // set the game instructions to active. 
+        uiInstructions.SetActive(false); // set the ui instructions to inactive.
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public void backTomenu() // when the back to menu button is pressed.
+    {
+
+
+        Application.LoadLevel(backToMenu); // load the main menu.
+
+    }
+
+    public void nextPage() // when the next page button is pressed.
+    {
+
+        gameInstructionsCanvas.SetActive(false); // turn the game instructions off.
+        uiInstructions.SetActive(true); // display the ui instructions.
+
+
+    }
+
+    public void previousPage() // when the previous page button is pressed.
+    {
+        uiInstructions.SetActive(false); // set the ui instructions to false.
+        gameInstructionsCanvas.SetActive(true); // set the game instructions to acive. 
+
+
+    }
+
+    public void continueToGame() // when the continue to game button is pressed.
+    {
+
+        Application.LoadLevel(tutorial); // load the tutorial level. 
+
+    }
+
+
+
+}
